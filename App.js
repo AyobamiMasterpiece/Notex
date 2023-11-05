dc5807ee4c1b53b8e16654c58f9b1e5e18b524a8
AyobamiMasterpiece <ayotibami14@gmail.com>
2023-11-05 06:28:25 -0800

    'stable-app'

--- App.js
+++ App.js
@@ -1,20 +1,109 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableNativeFeedback,
+} from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  const [search, setSearch] = useState("");
+  const [isNote, setIsNote] = useState(false);
+  function handleSearchChange(text) {
+    setSearch(text);
+    console.log(text);
+  }
+  function handleNoteNodal() {
+    setIsNote(!isNote);
+    console.log("ll");
+  }
+
+  // Set dark-colored icon style
+
+  return !isNote ? (
+    <View style={styles.app}>
+      <View style={styles.title}>
+        <Text style={styles.appTitle}>Notes</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <Icon name="search" size={20} color="white" />
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Search notes"
+          onChangeText={handleSearchChange}
+          value={search}
+        ></TextInput>
+      </View>
+
+      <TouchableNativeFeedback
+        onPress={() => {
+          handleNoteNodal();
+        }}
+        background={TouchableNativeFeedback.Ripple("grey", true, 30)}
+      >
+        <View style={styles.plus}>
+          <View>
+            <Icon name="plus" size={30} color="white" />
+          </View>
+        </View>
+      </TouchableNativeFeedback>
     </View>
+  ) : (
+    <View style={styles.modalnote}></View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  app: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+  },
+  appTitle: {
+    color: "white",
+    marginTop: 40,
+    fontSize: 30,
+    marginBottom: 15,
+  },
+  title: {
+    width: "90%",
+    marginTop: 30,
+
+    alignSelf: "center",
+  },
+  searchContainer: {
+    backgroundColor: "grey",
+    flexDirection: "row",
+
+    width: "90%",
+    alignSelf: "center",
+    padding: 5,
+    alignItems: "center",
+    borderRadius: 20,
+  },
+  searchInput: {
+    color: "white",
+    fontSize: 12,
+    width: "80%",
+    marginLeft: 10,
+  },
+  plus: {
+    backgroundColor: "grey",
+    alignSelf: "center",
+    padding: 10,
+    width: 50,
+    height: 50,
+    alignItems: "center",
+    position: "absolute",
+    bottom: 80,
+    right: 15,
+    borderRadius: 50,
+  },
+  modalnote: {
+    flex: 1,
+    backgroundColor: "red",
   },
 });
